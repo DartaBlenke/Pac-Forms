d3d23bace59daff45f89c07d16b7872678ec9253
DartaBlenke <dartagnan.blenke17@gmail.com>
2022-05-23 20:17:36 -0300

    tabela de produtos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,64 @@
 <template>
-  <h1>Home Page</h1>
+<div class="d-flex flex-column">
+  <h1
+  class="mt-9 ml-16 mb-9"
+  >Lista de Produtos</h1>
+  <v-data-table
+    :headers="headers"
+    :items="products"
+    :items-per-page="5"
+    class="mx-14"
+  >
+  </v-data-table>
+</div>
 </template>
 
 <script>
-
   export default {
     name: 'Home',
+    model: {},
 
     data: () => {
+      return {
+        headers: [
+          { text: 'Produto', value: 'produto' },
+          { text: 'Quantidade', value: 'quantidade' },
+          { text: 'Tipo', value: 'tipo' },
+          { text: 'Média', value: 'media' },
+          { text: 'Perecível', value: 'perecivel' },
+          { text: 'Previsão', value: 'previsao' },
+          { text: 'Status', value: 'status'}
+        ],
+        products: [
+          {
+            produto: 'Leite',
+            quantidade: 3,
+            tipo: 'Comprado',
+            media: '2L / dia',
+            perecivel: 'Sim',
+            previsao: '24/05/22',
+            status: 'Acabando'
+          },
+          {
+            produto: 'Leite',
+            quantidade: 3,
+            tipo: 'Comprado',
+            media: '2L / dia',
+            perecivel: 'Sim',
+            previsao: '24/05/22',
+            status: 'Acabando'
+          },
+          {
+            produto: 'Leite',
+            quantidade: 3,
+            tipo: 'Comprado',
+            media: '2L / dia',
+            perecivel: 'Sim',
+            previsao: '24/05/22',
+            status: 'Acabando'
+          },
+        ],
+      }
     },
   }
 </script>
